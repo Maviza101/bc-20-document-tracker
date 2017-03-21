07ef5c31e33e12008761e334f53458233b1fa900
Folusho <folusho@lanthanidesoft.com>
2017-03-21 14:32:48 +0100

    improve module import and routing

--- app.js
+++ app.js
@@ -1,22 +1,20 @@
 var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+// uncomment after placing your favicon in /public
+//var favicon = require('serve-favicon');
+
+// NOTE: On ALL routes, always, ALWAYS check that this user is logged in.
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+
 var expressApp = express();
 
 expressApp.set('port', (process.env.PORT || 5000));
 
-expressApp.use(express.static(__dirname + '/public'));
+expressApp.use(express.static(path.join(__dirname, '/public')));
 
-// views is directory for all template files
-expressApp.set('views', __dirname + '/views');
+// view engine setup
+expressApp.set('views', path.join(__dirname, 'views'));
 expressApp.set('view engine', 'ejs');
-
-expressApp.get('/', function(request, response) {
-  response.render('pages/index')
-});
-
-expressApp.get('/cool', function(request, response) {
-  response.send(cool());
-});
-
-expressApp.listen(expressApp.get('port'), function() {
-  console.log('Node app is running on port', expressApp.get('port'));
-});
