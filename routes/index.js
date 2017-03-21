d6e666c06e9c199d08a3851876da07d2f7dc0dfd
Folusho <folusho@lanthanidesoft.com>
2017-03-21 23:40:08 +0100

    update values to be displayed in view

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
+'use strict';
+
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var placeholderValues = {
+      pageTitle: 'Andela Document Tracker',
+      welcomeMessage: 'Welcome to the Andela Document Tracker.'
+  };
+  res.render('index', placeholderValues);
 });
 
 module.exports = router;
