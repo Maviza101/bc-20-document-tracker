5e13dfe5d4f069e562243063e398e6cd1df5aa54
Folusho <folusho@lanthanidesoft.com>
2017-03-23 16:14:43 +0100

    bug fixes; improve code structure

--- public/scripts/initFirebase.js
+++ public/scripts/initFirebase.js
@@ -1,11 +1,11 @@
 // This is reusable code.
 function initFirebase() {
   var config = {
-      apiKey: "AIzaSyDTLsprvv9GcYCmMHygGph-fXsA1BvEwF4",
-      authDomain: "document-tracker-cohort-20.firebaseapp.com",
-      databaseURL: "https://document-tracker-cohort-20.firebaseio.com",
-      storageBucket: "document-tracker-cohort-20.appspot.com",
-      messagingSenderId: "409648985148"
+      apiKey: 'AIzaSyDTLsprvv9GcYCmMHygGph-fXsA1BvEwF4',
+      authDomain: 'document-tracker-cohort-20.firebaseapp.com',
+      databaseURL: 'https://document-tracker-cohort-20.firebaseio.com',
+      storageBucket: 'document-tracker-cohort-20.appspot.com',
+      messagingSenderId: '409648985148'
   };
   firebase.initializeApp(config);
   return firebase;
