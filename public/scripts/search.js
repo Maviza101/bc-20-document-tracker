6a40001fc23432ab1fafff22bb511b7b05851d51
Folusho <folusho@lanthanidesoft.com>
2017-03-23 22:24:16 +0100

    add script specific to the search route

---
+++ public/scripts/search.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+  var firebase = initFirebase();
+  var database = firebase.database();
+  
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      
+    } else {
+      // Redirect non-signed in users to the homepage.
+      window.location.replace('/');
+      return;
+    }
+  });
+  
+  $('#search-department-btn').on('click', this, function() {
+    var departmentInput = $('.single-department:radio:checked');
+    var department = departmentInput.val();
+    console.log(department);
+    
+    var departmentRef = database.ref('documents/' + department);
+    database.ref('documents/' + department).once('value', function(snapshot) {
+    snapshot.forEach(function(childSnapshot) {
+    });
+});
+  });
+});
