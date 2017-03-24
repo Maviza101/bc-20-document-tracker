f34a1586bb597f16c731f5a99843d9fca51ed878
Folusho <folusho@lanthanidesoft.com>
2017-03-24 08:04:45 +0100

    implement searching by department, keyword or title

--- public/scripts/search.js
+++ public/scripts/search.js
@@ -21,9 +21,67 @@ $(document).ready(function() {
     var department = departmentInput.val();
     
     var departmentRef = database.ref('documents/' + department);
-    database.ref('documents/' + department).once('value', function(snapshot) {
-    snapshot.forEach(function(childSnapshot) {
+    departmentRef.once('value', function(snapshot) {
+      var databaseRecords = snapshot.val();
+      updateSearchResults(department, databaseRecords, 'department-search-results-target');
     });
-});
   });
+  
+  $('#search-keyword-btn').on('click', this, function() {
+    var keywordInput = $('#search-keyword');
+    var keyword = keywordInput.val().trim();
+    
+    var keywordRef = database.ref('keywords/' + keyword);
+    keywordRef.once('value', function(snapshot) {
+      var databaseRecords = snapshot.val();
+      updateSearchResults(keyword, databaseRecords, 'keyword-search-results-target');
+    });
+  });
+  
+  $('#search-title-btn').on('click', this, function() {
+    var titleInput = $('#search-title');
+    var title = titleInput.val().trim();
+    
+    var titleRef = database.ref('titles/' + title);
+    console.log(titleRef);
+    titleRef.once('value', function(snapshot) {
+      var databaseRecords = snapshot.val();
+      updateSearchResults(title, databaseRecords, 'title-search-results-target');
+    });
+  });
+  
+  function updateSearchResults(department, records, targetId) {
+    var recordKeys = Object.keys(records);
+    var resultsDestination = $('#' + targetId);
+
+    for (var i = 0; i < recordKeys.length; i++) {
+      var key = recordKeys[i];
+      var record = records[key];
+      var row = $('<tr>');
+
+      var titleCell = $('<td>');
+      titleCell.text(record.title);
+
+      var urlCell = $('<td>');
+      var urlElement = $('<a>');
+      urlElement.text('Link');
+      urlElement.attr('href', record.url);
+      urlCell.append(urlElement);
+
+      var departmentCell = $('<td>');
+      departmentCell.text(department);
+
+      var keywordsCell = $('<td>');
+      keywordsCell.text(record.keywords);
+
+      row.append(titleCell)
+        .append(urlCell)
+        .append(departmentCell)
+        .append(keywordsCell);
+
+      resultsDestination.append(row);
+    }
+  }
+  
 });
+  
